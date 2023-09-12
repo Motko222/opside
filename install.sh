95c5dd26c6bcb0b19af946c89857252bdbcc0f54
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-12 10:42:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,13 @@ rm testnet-auto-install-v3.tar.gz
 chmod +x -R ./testnet-auto-install-v3
 cd ./testnet-auto-install-v3
 ./install-ubuntu-1.0.sh
+
+FILE=~/config/opside.sh
+if [ -f "$FILE" ]; then
+    echo "$FILE exists, skipping creation..."
+else 
+    echo "$FILE does not exist, creating..."
+    echo "#!/bin/bash" > $FILE
+    echo "id=optional" >> $FILE
+    echo "path=/root/testnet-auto-install-v3/opside-chain" >> $FILE
+fi
