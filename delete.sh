c2c05eb4962fa8a8d94976f9935f43a0fadbb571
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-06 23:22:11 +0100

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -5,8 +5,8 @@ source ~/scripts/opside/config/env
 read -p "Sure?" c
  case $c in
  y|Y)
-  rm /root/testnet-auto-install-v3
-  rm /root/scripts/opside
+  rm -r /root/testnet-auto-install-v3
+  rm -r /root/scripts/opside
   echo "Done."
  ;;
  esac
